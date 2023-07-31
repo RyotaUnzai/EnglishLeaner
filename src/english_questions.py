5abee5354c5b83983bcf833727e061d63426d3c9
ryota.unzai <ryota-unzai@capcom.com>
2023-07-31 17:51:49 +0900

    sort問題の追加

--- src/english_questions.py
+++ src/english_questions.py
@@ -1,13 +1,17 @@
 import random
+import textwrap
+import os
 import sys
 from datetime import date
 from pathlib import Path
 
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtCore
 
-from model.questions import PATH_LOCALAPPDATA, Questions
+from model.questions import Questions
 from view.questions_view import Questions_Window
 
+
+os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
 QSS_PATH = Path(__file__).parent / "view" / "questions_view.qss"
 RESOURCE_PATH = Path(__file__).parent.with_name("resource").absolute()
 
@@ -19,17 +23,67 @@ class Connect:
     def __init__(self) -> None:
         self.window = Questions_Window()
         self.model = Questions(RESOURCE_PATH / "question_001.json")
-        # print(self.model.items)
-        self.__setNumbers()
-        self._setUI()
-
-    def _setUI(self) -> None:
         self.window.setStyleSheet(self.__loadQSS())
-        self.window.lineEditAnswer.returnPressed.connect(self.__result)
-        self.window.pushButtonNext.clicked.connect(self.__nextQuestion)
-        self.window.pushButtonPrev.clicked.connect(self.__prevQuestion)
-        
-        self.window.labelCurrentNumber.setText(f"{self.__current} / {self.model.item_count}")
+        # print(self.model.items)
+        # self._setUI()
+        self.__setNumbers()
+        self.window.menu.pushButtonSelection.clicked.connect(self._setSelectionUI)
+        self.window.menu.pushButtonSort.clicked.connect(self._setSortUI)
+
+    def _setSortUI(self) -> None:
+        self.__setSortQuestion(self.__numbers[self.__current])
+        self.window.sort.lineEditAnswer.returnPressed.connect(self.__sortResult)
+        self.window.sort.pushButtonNext.clicked.connect(self.__nextSortQuestion)
+        self.window.sort.pushButtonPrev.clicked.connect(self.__prevSortQuestion)
+
+    def _setSelectionUI(self) -> None:
+        self.__setSelectionQuestion(self.__numbers[self.__current])
+        self.window.selection.lineEditAnswer.returnPressed.connect(self.__selectionResult)
+        self.window.selection.pushButtonNext.clicked.connect(self.__nextSelectionQuestion)
+        self.window.selection.pushButtonPrev.clicked.connect(self.__prevSelectionQuestion)
+
+    def insert_newlines(self, string, every=60):
+        lines = ""
+        newlines = []
+        print(string)
+
+        for i in range(0, len(lines), every):
+            newlines.append(lines[i:i+every])
+
+        return "\n".join(newlines)
+
+    def __setSortQuestion(self, num: int) -> None:
+        self.model.num = num
+        self.window.sort.labelCount.setText(f"{self.model.count}")
+        self.window.sort.labelJapanese.setText(self.model.jp)
+        words = self.model.en.replace(".", "").split(" ")
+        random.shuffle(words)
+        numbered_words = [f"{i+1}.{word},  " for i, word in enumerate(words)]
+        shuffled_text = " ".join(numbered_words)
+        formatted_text = "\n".join(textwrap.wrap(shuffled_text, width=60))
+        formatted_text = f"{formatted_text.replace('.', '. ')}"[:-1]
+
+        self.window.sort.labelQuestion.setText(formatted_text)
+        self.window.sort.labelCurrentNumber.setText(f"{self.__current+1} / {self.model.item_count}")
+        self.window.sort.textEditExplanation.setPlainText(
+            f"{self.model.en}\n{self.model.jp}\n\n{self.model.explanation}")
+        self.window.sort.labelResult.setText(self.model.selections[self.model.answer])
+        self.window.sort.textEditExplanation.hide()
+        self.window.sort.labelResult.hide()
+
+    def __setSelectionQuestion(self, num: int) -> None:
+        self.model.num = num
+        self.window.selection.labelCount.setText(f"{self.model.count}")
+        self.window.selection.labelJapanese.setText(self.model.jp)
+        self.window.selection.labelQuestion.setText(self.model.question)
+        self.window.selection.labelCurrentNumber.setText(f"{self.__current+1} / {self.model.item_count}")
+        for num, selection in enumerate(self.model.selections):
+            eval(f"self.window.selection.radioButton_{num}.setText('{selection}')")
+        self.window.selection.textEditExplanation.setPlainText(
+            f"{self.model.en}\n{self.model.jp}\n\n{self.model.explanation}")
+        self.window.selection.labelResult.setText(self.model.selections[self.model.answer])
+        self.window.selection.textEditExplanation.hide()
+        self.window.selection.labelResult.hide()
 
     def __loadQSS(self) -> str:
         with open(QSS_PATH.absolute().as_posix(), encoding="utf-8") as f:
@@ -38,15 +92,30 @@ class Connect:
     def __setNumbers(self) -> None:
         self.__numbers = [*range(self.model.item_count)]
         random.shuffle(self.__numbers)
-        self.__setQuestion(self.__numbers[self.__current])
 
-    def __result(self) -> None:
+    def __sortResult(self) -> None:
+        self.window.sort.textEditExplanation.show()
+        self.window.sort.labelResult.show()
+        self.window.setStyleSheet(self.__resultSortStyleSheet())
+
+    def __resultSortStyleSheet(self) -> str:
+        self.model.datetime = date.today().isoformat()
+        if self.model.en == self.window.sort.lineEditAnswer.text():
+            self.model.count += 1
+            return self.__loadQSS() + "\nQLineEdit#lineEditAnswer{color: #38A46E;}"
+
+        if self.model.count > 0:
+            self.model.count -= 1
+
+        return self.__loadQSS() + "\nQLineEdit#lineEditAnswer{color: #E01450;}"
+
+    def __selectionResult(self) -> None:
         isChecked: bool = False
-        self.window.textEditExplanation.show()
-        self.window.labelResult.show()
+        self.window.selection.textEditExplanation.show()
+        self.window.selection.labelResult.show()
         count = 0
         while count:
-            exec(f"locals()['result'] = self.window.radioButton_{count}.isChecked()")
+            exec(f"locals()['result'] = self.window.selection.radioButton_{count}.isChecked()")
             if locals()['result']:
                 isChecked = True
                 break
@@ -55,14 +124,14 @@ class Connect:
             count += 1
         if not isChecked:
             try:
-                num = self.model.selections.index(self.window.lineEditAnswer.text())
-                eval(f"self.window.radioButton_{num}.setChecked(True)")
+                num = self.model.selections.index(self.window.selection.lineEditAnswer.text())
+                eval(f"self.window.selection.radioButton_{num}.setChecked(True)")
             except ValueError:
                 pass
-        self.window.setStyleSheet(self.__resultStyleSheet())
+        self.window.setStyleSheet(self.__resultSelectionStyleSheet())
 
-    def __resultStyleSheet(self) -> str:
-        exec(f"locals()['result'] = self.window.radioButton_{self.model.answer}.isChecked()")
+    def __resultSelectionStyleSheet(self) -> str:
+        exec(f"locals()['result'] = self.window.selection.radioButton_{self.model.answer}.isChecked()")
         self.model.datetime = date.today().isoformat()
         if locals()['result']:
             self.model.count += 1
@@ -76,59 +145,81 @@ class Connect:
     def __setData(self, path: Path) -> None:
         self.model.path = path
 
-    def __initUI(self) -> None:
+    def __initSortUI(self) -> None:
+        pass
+
+    def __initSelectionUI(self) -> None:
         self.window.buttonGroup.setExclusive(False)
         for num in range(4):
-            exec(f"self.window.radioButton_{num}.setChecked(False)")
+            exec(f"self.window.selection.radioButton_{num}.setChecked(False)")
         self.window.buttonGroup.setExclusive(True)
-        self.window.lineEditAnswer.setText("")
+        self.window.selection.lineEditAnswer.setText("")
 
-    def __nextQuestion(self) -> None:
+    def __nextSortQuestion(self) -> None:
         self.window.setStyleSheet(self.__loadQSS())
-        self.__initUI()
+        self.__initSortUI()
         if self.__current < self.model.item_count:
             self.__current += 1
-            self.__setQuestion(self.__numbers[self.__current])
+            self.__setSortQuestion(self.__numbers[self.__current])
 
         if self.__current > 0:
-            self.window.pushButtonPrev.setEnabled(True)
+            self.window.sort.pushButtonPrev.setEnabled(True)
 
         if self.__current == self.model.item_count - 1:
-            self.window.pushButtonNext.setEnabled(False)
+            self.window.sort.pushButtonNext.setEnabled(False)
         else:
-            self.window.pushButtonNext.setEnabled(True)
+            self.window.sort.pushButtonNext.setEnabled(True)
 
-    def __prevQuestion(self) -> None:
+    def __prevSortQuestion(self) -> None:
         self.window.setStyleSheet(self.__loadQSS())
-        self.__initUI()
+        self.__initSortUI()
         if self.__current <= self.model.item_count \
                 and self.__current > -1:
             self.__current -= 1
-            self.__setQuestion(self.__numbers[self.__current])
+            self.__setSortQuestion(self.__numbers[self.__current])
 
         if self.__current != self.model.item_count:
-            self.window.pushButtonNext.setEnabled(True)
+            self.window.sort.pushButtonNext.setEnabled(True)
 
         if self.__current == 0:
-            self.window.pushButtonPrev.setEnabled(False)
+            self.window.sort.pushButtonPrev.setEnabled(False)
         else:
-            self.window.pushButtonPrev.setEnabled(True)
+            self.window.sort.pushButtonPrev.setEnabled(True)
 
-    def __setQuestion(self, num: int) -> None:
-        self.model.num = num
-        self.window.labelCount.setText(f"{self.model.count}")
-        self.window.labelQuestion.setText(self.model.question)
-        self.window.labelCurrentNumber.setText(f"{self.__current} / {self.model.item_count}")
-        for num, selection in enumerate(self.model.selections):
-            eval(f"self.window.radioButton_{num}.setText('{selection}')")
-        self.window.textEditExplanation.setPlainText(f"{self.model.en}\n{self.model.jp}\n\n{self.model.explanation}")
-        self.window.labelResult.setText(self.model.selections[self.model.answer])
-        self.window.textEditExplanation.hide()
-        self.window.labelResult.hide()
+    def __nextSelectionQuestion(self) -> None:
+        self.window.setStyleSheet(self.__loadQSS())
+        self.__initSelectionUI()
+        if self.__current < self.model.item_count:
+            self.__current += 1
+            self.__setSelectionQuestion(self.__numbers[self.__current])
 
+        if self.__current > 0:
+            self.window.selection.pushButtonPrev.setEnabled(True)
+
+        if self.__current == self.model.item_count - 1:
+            self.window.selection.pushButtonNext.setEnabled(False)
+        else:
+            self.window.selection.pushButtonNext.setEnabled(True)
+
+    def __prevSelectionQuestion(self) -> None:
+        self.window.setStyleSheet(self.__loadQSS())
+        self.__initSelectionUI()
+        if self.__current <= self.model.item_count \
+                and self.__current > -1:
+            self.__current -= 1
+            self.__setSelectionQuestion(self.__numbers[self.__current])
+
+        if self.__current != self.model.item_count:
+            self.window.selection.pushButtonNext.setEnabled(True)
+
+        if self.__current == 0:
+            self.window.selection.pushButtonPrev.setEnabled(False)
+        else:
+            self.window.selection.pushButtonPrev.setEnabled(True)
 
 def main() -> None:
-    app = QtWidgets.QApplication(sys.argv)
+    app = QtWidgets.QApplication([])
+    app.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
     ex = Connect()
     ex.window.show()
     sys.exit(app.exec_())
--- src/view/questions_view.py
+++ src/view/questions_view.py
@@ -4,13 +4,20 @@ from pathlib import Path
 from PySide2 import QtWidgets
 from PySide2.QtUiTools import loadUiType
 
-UI_PATH = Path(__file__).with_name("questions_view.ui")
+WINDOW_UI_PATH = Path(__file__).with_name("questions_view.ui")
 
-_GENERATED_CLASS, _ = loadUiType(UI_PATH.as_posix())
+MENU_UI_PATH = Path(__file__).with_name("questions_menu.ui")
+SELECTION_UI_PATH = Path(__file__).with_name("questions_selection.ui")
+SORT_UI_PATH = Path(__file__).with_name("questions_sort.ui")
+_GENERATED_WINDOW_CLASS, _ = loadUiType(WINDOW_UI_PATH.as_posix())
+_GENERATED_MENU_CLASS, _ = loadUiType(MENU_UI_PATH.as_posix())
+_GENERATED_SELECTION_CLASS, _ = loadUiType(SELECTION_UI_PATH.as_posix())
+_GENERATED_SORT_CLASS, _ = loadUiType(SORT_UI_PATH.as_posix())
 
 
-class Questions_Window(_GENERATED_CLASS, QtWidgets.QMainWindow):
+class Questions_Selection(_GENERATED_SELECTION_CLASS, QtWidgets.QWidget):
     lineEditAnswer: QtWidgets.QLineEdit
+    labelJapanese: QtWidgets.QLabel
     labelQuestion: QtWidgets.QLabel
     labelResult: QtWidgets.QLabel
     labelCount: QtWidgets.QLabel
@@ -22,19 +29,95 @@ class Questions_Window(_GENERATED_CLASS, QtWidgets.QMainWindow):
     radioButton_3: QtWidgets.QRadioButton
     pushButtonNext: QtWidgets.QPushButton
     pushButtonPrev: QtWidgets.QPushButton
+    pushButtonBack: QtWidgets.QPushButton
     menubar: QtWidgets.QMenuBar
     menuFile: QtWidgets.QMenu
     actionOpen: QtWidgets.QAction
     statusbar: QtWidgets.QStatusBar
     buttonGroup: QtWidgets.QButtonGroup
 
+    def __init__(self, parent=None, *args, **kwargs) -> None:
+        super(Questions_Selection, self).__init__(parent, *args, **kwargs)
+        self.setupUi(self)
+        self.buttonGroup = QtWidgets.QButtonGroup()
+        for num in range(4):
+            exec(f"self.buttonGroup.addButton(self.radioButton_{num})")
+
+
+class Questions_Sort(_GENERATED_SORT_CLASS, QtWidgets.QWidget):
+    lineEditAnswer: QtWidgets.QLineEdit
+    labelJapanese: QtWidgets.QLabel
+    labelQuestion: QtWidgets.QLabel
+    labelResult: QtWidgets.QLabel
+    labelCount: QtWidgets.QLabel
+    labelCurrentNumber: QtWidgets.QLabel
+    textEditExplanation: QtWidgets.QTextEdit
+    pushButtonNext: QtWidgets.QPushButton
+    pushButtonPrev: QtWidgets.QPushButton
+    pushButtonBack: QtWidgets.QPushButton
+    menubar: QtWidgets.QMenuBar
+    menuFile: QtWidgets.QMenu
+    actionOpen: QtWidgets.QAction
+    statusbar: QtWidgets.QStatusBar
+    buttonGroup: QtWidgets.QButtonGroup
+
+    def __init__(self, parent=None, *args, **kwargs) -> None:
+        super(Questions_Sort, self).__init__(parent, *args, **kwargs)
+        self.setupUi(self)
+
+
+class Questions_Menu(_GENERATED_MENU_CLASS, QtWidgets.QWidget):
+    pushButtonSelection: QtWidgets.QPushButton
+    pushButtonSort: QtWidgets.QPushButton
+
+    def __init__(self, parent=None, *args, **kwargs) -> None:
+        super(Questions_Menu, self).__init__(parent, *args, **kwargs)
+        self.setupUi(self)
+
+
+class Questions_Window(_GENERATED_WINDOW_CLASS, QtWidgets.QMainWindow):
+    menu: Questions_Menu
+    sort: Questions_Sort
+    selection: Questions_Selection
+    verticalLayout: QtWidgets.QVBoxLayout
+
     def __init__(self, *args, **kwargs) -> None:
         super(Questions_Window, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.menu = Questions_Menu(self)
+        self.sort = Questions_Sort(self)
+        self.selection = Questions_Selection(self)
         self.__initUI()
 
     def __initUI(self) -> None:
         self.setWindowTitle("English Questions")
-        self.buttonGroup = QtWidgets.QButtonGroup()
-        for num in range(4):
-            exec(f"self.buttonGroup.addButton(self.radioButton_{num})")
+        self.showSectionMenu()
+        self.verticalLayout.addWidget(self.sort)
+        self.verticalLayout.addWidget(self.selection)
+        self.verticalLayout.addWidget(self.menu)
+
+        self.menu.pushButtonSelection.clicked.connect(self.showSectionSelection)
+        self.menu.pushButtonSort.clicked.connect(self.showSectionSort)
+        self.sort.pushButtonBack.clicked.connect(self.showSectionMenu)
+        self.selection.pushButtonBack.clicked.connect(self.showSectionMenu)
+
+    def showSectionSort(self) -> None:
+        geometry = self.geometry()
+        self.selection.hide()
+        self.menu.hide()
+        self.sort.show()
+        self.setGeometry(geometry)
+
+    def showSectionSelection(self) -> None:
+        geometry = self.geometry()
+        self.sort.hide()
+        self.menu.hide()
+        self.selection.show()
+        self.setGeometry(geometry)
+
+    def showSectionMenu(self) -> None:
+        geometry = self.geometry()
+        self.selection.hide()
+        self.sort.hide()
+        self.menu.show()
+        self.setGeometry(geometry)
